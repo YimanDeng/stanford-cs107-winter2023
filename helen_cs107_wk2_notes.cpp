3792ec35a565a8fcbde9f032588e3b72db7dfa88
Yiman Deng <109169076+YimanDeng@users.noreply.github.com>
2023-01-17 17:29:22 -0800

    Create helen_cs107_wk2_notes.cpp

---
+++ helen_cs107_wk2_notes.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std
+  
+  /*
+  assignment tips
+    - Declaring a literal (when you want something different from the default)
+      put a U after a number literal to make it unsigned. For instance, 1U means the literal 1 as an unsigned number.
+      put an L after a number literal to make it a long (64 bits)
+    - Unix tool (gdb)
+  */
