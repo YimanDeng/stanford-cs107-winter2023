7ca23737571f6954401e60b33bf027deedc9f91a
Yiman Deng <109169076+YimanDeng@users.noreply.github.com>
2023-01-27 13:28:20 -0800

    Update helen_cs107_wk3_notes.c

--- helen_cs107_wk3_notes.c
+++ helen_cs107_wk3_notes.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /*
-1. C pointers
+1. C Pointers
   Just a number indicating memory addresses. Myth machines pointers are 8-byte long
   Pointers are reference to values. One pointer can refer to any size data stucture
   allows reserving new memory during a program
@@ -30,7 +30,7 @@ printf("Mystery: %c\n", mystery); // prints out 'C'
   Pointer arithmetic
     - plus, minus ++ means incrementing address by size of an element of type of array, compiler knows how wide the type is.
   You can't get the size of an array after you pass it in a function
-  Arrays are passed by reference, means passing a pointer. So you can modify the elements passed in; use const to avoid.
+  ! Arrays are passed by reference, means passing a pointer. So you can modify the elements passed in; use const to avoid.
   C doesn't check for end of array, can walk off the end
   char *argv[] is an array of pointers
 */
@@ -45,3 +45,69 @@ void *memove(void *dest, const void *src, size_t n); // allows overlapping memor
 
 char s1[] = "string in an array"; // allows modifying, declaration of a string as array
 char *s2 = "string literal with pointer"; // no modifying, attempt would result in seg fault
+
+// swap function for array, sets values directly
+void swapA(int *arr, int index_x, int index_y) { // first arg identical to arr[], because once you pass in an array to a function in c, you get a ptr to the first elem in the arr
+  int tmp = *(arr + index_x);
+  *(arr + index_x) = *(arr + index_y);
+  *(arr + index_y) = temp;
+}
+// can do same ting with memmove
+void swapB(int *arr, int index_x, int index_y) {
+  int tmp;
+  memmove(&tmp, arr + index_x, sizeof(int)); // this works because we know the size of the elements in the array, int
+  memmove(arr + index_x, arr + index_y, sizeof(int));
+  memmove(arr + index_y, arr + index_x, sizeof(int));
+}
+// Now, don't swap values directly but swap pointers. 
+// look at argv which is an array of char*. write a fucntion to swap two pointers in the array
+void swapC(char** arr) {
+  char* tmp = *(arr + index_x);
+  *(arr + index_x) = *(arr + index_y);
+  *(arr + index_y) = temp; // very similar to swapA, but faster since we are only swapping pointers.
+}
+
+// Double pointers
+// write a function that returns the char in a string pointed by p, and updates string ptr to point to the next char
+char* next(char** p) {
+  char next = *(p)[0];
+  *(p)++; // string ptr getting updated
+  return next;
+}
+
+char* pB = "hello";
+printf("next(&pB): %c\n", next(&pB));
+
+/*
+3. x86-64 Memory Layout
+  2 different areas of memory: stack & heap
+    -> every program by default has access to 8Mb of stack memory, stack grows downwards. When exceeds its amount, stackoverflow.
+    -> shared library/data
+    -> heap, program requests memory frmo the operating system (malloc, calloc, realloc, free). grows upwards
+    -> global data
+    -> text (machine code)
+  Stack allocation
+    - local variables and arrays kept on the stack
+  Parameters
+    - parameters can be put on the stack, and behave like local variables.
+  Why we like stack allocation
+    - fast. Allocating space on the stack is efficient because your program already has access to the memory.
+    - convenient. The scope (lifetime) of the data is inside the function, so it keeps things tidy.
+    - type safety. You are controlling the type of the variables, and therefore the compiler can do lots of checks on the data. not always the case with heap memory.
+  Why we don't like stack allocation
+    - isn't that plentiful
+    - size fixed at declaration, no option to resize
+    - limited scope
+  Dynamic allocation
+    - in C++ when you use new and delete
+    - malloc, memoery allocation for a specified number of bytes. if malloc return NULL, not enough memory to request
+    - free, ptr must point to a previously allocated block (or it can be NULL). Once a program frees memory, it cannot be used again. 
+      The pointer can be used to point elsewhere.
+*/
+int scores = malloc(20 * sizeof(int)); // 20 integers. malloc works in bytes
+int scores = calloc(20, sizeof(int)); // diff: it zeros out the memory for you.
+
+void* calloc(size_t count, size_t size);
+void* malloc(size_t size);
+void* realloc(void *ptr, size_t size);
+void free(void *ptr);
