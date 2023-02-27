cb17f4f650ee3d7507fe7eb5198df7abb55115ac
Yiman Deng <109169076+YimanDeng@users.noreply.github.com>
2023-02-27 12:55:09 -0800

    Create helen_cs107_wk8_notes.c

---
+++ helen_cs107_wk8_notes.c
@@ -0,0 +1,47 @@
+/*
+1. Allocating memory
+
+void *malloc(size_t size)
+  return pointer to memory >= requested size
+  failure returns NULL and sets errno
+  
+void free(void *p)
+  recycle memory block
+  p must be previous malloc/realloc call
+  
+void *realloc(void *p, size_t size)
+  changes size of block p, returns pointer to block (possibly different)
+  contents of new block unchanged up to min of old and new size
+  If new pointer not same as old pointer, the old block is freed
+  
+The heap allocator must be able to service an arbitrary sequence of malloc() and free()
+It must align blocks so that they satisfy all alignment requirements (e.g., 8-byte / 16 byte boundary)
+The allocator cannot move allocated blocks.
+
+Ideally, requests should be handled in constant itme, and not degrade to linear behavior
+The allocator must try for a tight space utilization. Should try to minimize fragmentation.
+Good locality, blocks are allocated close in time and space.
+Robust client errors should be recognized.
+Ease of implementation and maintenance. Use structs and typedef instead of having a lot of *(void **)
+
+Questions
+- How do we track the information in a block?
+- How do we organize/find free block?
+- How do we pick which free block from available options?
+- What do we do with excess space when allocating a block?
+- How do we recycle a freed block? 
+
+Options
+- A separate list
+- Block header
+  info in same memory area as the payload, but precedes the payload. (What's actually used)
+  Coalescing forward (can't do backward without footer)
+  The method demostrated is called "implicit free list", not really efficient
+  Finding free blocks is a linear search process. (first-fit)
+- next-fit
+- best-fit
+- Explicit free list
+  Also keep the meta data inside the heap. Have a pointer to the next and previous free block.
+  A linked list implementation.
+  Faster because you only have to traverse the free blocks.
+*/
